a8b1e420d914c77b51e2f236a65ec3880bc5e71a
nafeesalways <nafeezmy@gmail.com>
2025-12-07 21:51:49 +0600

    authController setup updated

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,20 +2,20 @@ import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { AuthRequest } from '../middleware/authMiddleware';
 
-// হার্ডকোডেড ক্রেডেনশিয়াল (অ্যাসাইনমেন্টের জন্য)
+
 const ADMIN_EMAIL = 'admin@test.com';
 const ADMIN_PASS = '123456';
 
 export const loginUser = (req: Request, res: Response) : any => {
   const { email, password } = req.body;
 
-  // 1. ক্রেডেনশিয়াল চেক
+  // check credential
   if (email !== ADMIN_EMAIL || password !== ADMIN_PASS) {
     return res.status(401).json({ success: false, message: 'Invalid email or password' });
   }
 
-  // 2. JWT টোকেন জেনারেট
-  // process.env.JWT_SECRET স্ট্রিং নিশ্চিত করা হচ্ছে
+  // 2. JWT token generate
+
   const secret = process.env.JWT_SECRET || 'fallback_secret_key';
   
   const token = jwt.sign(
@@ -24,7 +24,7 @@ export const loginUser = (req: Request, res: Response) : any => {
     { expiresIn: '1d' }
   );
 
-  // 3. কুকি সেট করা (লোকালহোস্টের জন্য অপ্টিমাইজড)
+  // 3. set cookie
 res.cookie('token', token, {
   httpOnly: true,
   secure: true, 
